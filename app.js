0a851f479a07b08ed6f6015c84ecf0320daeefb0
Dmitry Mitenkoff <d.mitenkoff@icloud.com>
2021-01-24 13:32:06 +1300

    Menu Slider and Modal in CSS

---
+++ app.js
@@ -0,0 +1,26 @@
+const toggle = document.querySelector("#toggle");
+const close = document.querySelector("#close");
+const open = document.querySelector("#open");
+const modal = document.querySelector("#modal");
+
+// Toggle navigation
+toggle.addEventListener("click", () => {
+    document.body.classList.toggle("show-nav");
+});
+
+// Show modal
+open.addEventListener("click", () => {
+    modal.classList.add("show-modal");
+});
+
+// Hide modal
+close.addEventListener("click", () => {
+    modal.classList.remove("show-modal");
+});
+
+// Hide modal on outside click
+window.addEventListener("click", event => {
+    if (event.target === "modal") {
+        modal.classList.remove("show-modal");
+    } 
+});
